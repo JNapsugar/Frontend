7244b88878f604e883ed376e3cdf033bee159cf3
JNapsugar <147054654+JNapsugar@users.noreply.github.com>
2024-12-03 08:08:28 +0100

    Deploy

--- darts/src/App.jsx
+++ darts/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, NavLink, Routes, Route } from "react-router-dom";
 import { DartsListPage } from "./DartsListPage";
+import {DartsSingle} from "./DartsSingle";
+import {DartsCreate} from "./DartsCreate";
+import { DartsMod } from './DartsMod';
+import { DartsDel } from './DartsDel';
 import './App.css';
 
 export const App =()=> {
@@ -13,12 +17,19 @@ export const App =()=> {
               <NavLink to={'/'} className={({isActive}) => "nav-link" + (isActive ? "active" : "")}>
                 <span className="nav-link">Dartsozók</span>
               </NavLink>
+              <li className='nav-item'>
+              <NavLink className="nav-link" to="/create-darts">Új dartsozó</NavLink>
+            </li>
               </li>
           </ul>
         </div>
       </nav>
       <Routes>
         <Route path="/" exact element={<DartsListPage />} />
+        <Route path="/darts/:dartsId" element={<DartsSingle />} />
+        <Route path="/create-darts" element={<DartsCreate />} />
+        <Route path='/mod-darts/:dartsId' element={<DartsMod/>} />
+        <Route path='/del-darts/:dartsId' element={<DartsDel/>} />
       </Routes>
     </Router>
   );
